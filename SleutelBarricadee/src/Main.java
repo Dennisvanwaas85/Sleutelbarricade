130e599a12acca249beb841319ad064cb07214df
regob95 <rezagobin@hotmail.com>
2019-03-18 15:56:34 +0100

    test 2
    Is er iets nieuws bij? Test 2

--- SleutelBarricadee/src/Main.java
+++ SleutelBarricadee/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("dit is een test");
+        System.out.println("Krijgen jullie dit ook?");
 
     }
 }
